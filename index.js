778a226f916451e929c6f615cd3a2af8ae6eaa1a
adeyemi <aladesuyiadeyemi05@gmail.com>
2025-02-01 21:17:23 +0100

    add student router to main application

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const sequelize = require('./config/database');
 const authRouter = require('./routes/auth');
 const courseRepRouter = require('./routes/course_representative');
 const app = express();
+const studentRouter = require('./routes/student');
 app.use(express.json());
 // Middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(authRouter);
 app.use(courseRepRouter);
-
+app.use(studentRouter);
 
 
 sequelize
