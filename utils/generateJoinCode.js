a2d3e4401b01312737cd28277eec685412c8e76a
adeyemi <aladesuyiadeyemi05@gmail.com>
2025-02-01 21:04:03 +0100

    add utility function to generate random join codes for classrooms

---
+++ utils/generateJoinCode.js
@@ -0,0 +1,10 @@
+const generateJoinCode = () => {
+    try {
+        return Math.random().toString(36).substring(2, 7).toUpperCase(); 
+    } catch (error) {
+        console.error('Error generating join code:', error);
+        return null;
+    }
+};
+
+module.exports = { generateJoinCode };
